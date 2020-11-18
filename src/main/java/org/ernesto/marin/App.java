0e4cf6f36a057f32c86ccd97c432a8ea19d37627
ErnestoMot <erniesto2000@gmail.com>
2020-11-18 11:43:09 +0100

    Haciendo el XMLParser

--- src/main/java/org/ernesto/marin/App.java
+++ src/main/java/org/ernesto/marin/App.java
@@ -1,5 +1,15 @@
 package org.ernesto.marin;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -44,6 +54,8 @@ public class App
                         break;
 
                     case 5:
+                        System.out.println("Hasta la proximaaaaaaaa, piuuuuuum pown pown pown pown piiiiiuuum ");
+                        menu = 5;
                         break;
 
                     default:
@@ -64,4 +76,38 @@ public class App
         }while (menu!=5);
 
     }
+
+    public static void LeerFichero(){
+
+        File file = new File("Concesionario.xml");
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("coche");
+
+            for(int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+
+                if(nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("\nCoche id: " + eElement.getAttribute("id"));
+                    System.out.println("Marca: "
+                            + eElement.getElementsByTagName("marca").item(0).getTextContent());
+                    System.out.println("Modelo: "
+                            + eElement.getElementsByTagName("modelo").item(0).getTextContent());
+                    System.out.println("Cilindrada: "
+                            + eElement.getElementsByTagName("cilindrada").item(0).getTextContent());
+                }
+            }
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
