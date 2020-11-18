26257358dc3e95c9411143086965d81e02ab965a
ErnestoMot <erniesto2000@gmail.com>
2020-11-18 10:37:45 +0100

    Primer commit

---
+++ src/main/java/org/ernesto/marin/App.java
@@ -0,0 +1,17 @@
+package org.ernesto.marin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
